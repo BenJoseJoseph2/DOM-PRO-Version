d82b66b4dcfc8b5b0b4060e9b4e16bde0d9b718d
BenJoseJoseph02 <benjosejoseph563@gmail.com>
2024-04-21 22:59:24 +0530

    Intro Of DOM

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log("Hello We are Going to learn DOM In Pro Mode ...")
+
+let heading = document.getElementById('heading');
+console.log(heading)
+
+let heading2 = document.getElementsByClassName('.heading3')
+console.log(heading2)
+
+let paragraph = document.getElementsByTagName("p")
+console.log(paragraph);
+
+let fel = document.querySelector('p')
+console.log(fel)
+
+let allel = document.querySelectorAll('p')
+console.log(allel)
+
+
+console.log(document.body.lastChild)
+console.log(document.body.firstChild)
+
+let div = document.querySelector('div')
